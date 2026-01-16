6774f42d6e06c4f16f9e7797d90ec1d94638847d
Diksha6524 <diksha.232463201@vcet.edu.in>
2026-01-16 20:08:49 +0530

    Update cleaning.py
    added more lines of code in the same

--- cleaning.py
+++ cleaning.py
@@ -28,4 +28,72 @@ print(df.head())
 #spliting height and weight 
 df[['height','weight']]=df['Measurements'].str.split('/',expand=True)#we used expand=true cux we want two columns
 print(df[['height','weight']])
+
 print(df)
+print(df.info()) 
+# in o/p we noticed that this two columns height and weight were objecst we want them to be numeric values
+# and in the columsn of height and weight they hv 'cm' and 'kg' units with them which we dont want 
+# i wanna check if measurement column has same pattern that is all rows or va;ues in columns has / pattern or not
+
+
+
+#check / from all rows of mr=easurements
+print(df[~df['Measurements'].str.contains('/',na=False) & df['Measurements'].notna()]) #here we just seeing values which hv no / in it plus no na values,~ means negotiation     and it returns true or false value
+ # now i wanna see if i split just '180cm' by / whats output will i get?
+ #consider small dataset
+# small_dataset=pd.DataFrame(['180 cm','183 cm/ 150 kg','188 cm/ 120 kg','100 kg'],columns=['measures'])
+# print(small_dataset)
+
+# #now ill split it
+# small_dataset[['height','weight']]=small_dataset['measures'].str.split('/',expand=True)
+# small_dataset['weight'].fillna(small_dataset['height'],inplace=True) #this copies single value on both columns
+# print(small_dataset.head()) # in here 100 kgs went into height column rather then weight#
+# #op
+# #          measures  height   weight
+# # 0          180 cm  180 cm     None
+# # 1  183 cm/ 150 kg  183 cm   150 kg
+# # 2  188 cm/ 120 kg  188 cm   120 kg
+# # 3          100 kg  100 kg     None
+
+
+# # we need only cm into height column n so open
+# #therefore we use strip method
+
+# small_dataset['height']=pd.to_numeric(small_dataset['height'].str.strip(' cm'),errors='coerce')
+# small_dataset['weight']=pd.to_numeric(small_dataset['weight'].str.strip(' kg'),errors='coerce')
+# print(small_dataset)
+# #output
+# #          measures  height  weight
+# # 0          180 cm   180.0     NaN
+# # 1  183 cm/ 150 kg   183.0   150.0
+# # 2  188 cm/ 120 kg   188.0   120.0
+# # 3          100 kg     NaN   100.0
+
+
+
+# and in the columsn of height and weight they hv 'cm' and 'kg' units with them which we dont want
+df['height']=df['height'].str.extract(r'(\d+)') #r'(\d+)'-> means r means regulare expression and rest means we just need to extrac digtd from thet column srather then words like 'cm' and 'kgs' 
+# df['weight']=df['weight'].str.extract(r'(\d+)')# or me can do 
+df['weight']=df['weight'].str.split(' kg') #more efficient way to doit 
+
+print(df[['height','weight']])
+#o/p
+#          NaN      NaN
+# 1         183  [ 76, ]
+# 2         183  [ 76, ]
+# 3         168  [ 64, ]
+# 4         NaN      NaN
+# ...       ...      ...
+# 145495    167  [ 61, ]
+# 145496    168  [ 65, ]
+# 145497    163  [ 55, ]
+# 145498    166     None
+
+print(df.head(2))
+
+df['height']=pd.to_numeric(df['height'].str.strip(' cm'),errors='coerce')
+df['weight']=pd.to_numeric(df['weight'].str.strip(' kg'),errors='coerce')
+
+
+# print(df['Measurements'])#from this i can see some people hv na values and some just hv height 
+print(df.head)
